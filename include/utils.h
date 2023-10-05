130db41cea9f766b8f920cd2e7ee24f9d72d5126
Ace_Radom <sichenradomlyu@gmail.com>
2023-10-05 21:17:38 +0200

    add confirm_interrupt function

--- include/utils.h
+++ include/utils.h
@@ -31,12 +31,34 @@ namespace rena {
 
     template <typename _T>
     bool is_future_ready( const std::future<_T>& F );
+    bool confirm_interrupt( const std::string& msg , char y , char n );
 
 }; // namespace rena
 
 template <typename _T>
 bool rena::is_future_ready( const std::future<_T>& F ){
     return F.wait_for( std::chrono::seconds( 0 ) ) == std::future_status::ready;
+}
+
+bool rena::confirm_interrupt( const std::string& msg , char y , char n ){
+
+start_confirm:
+
+    std::cout << msg << " (" << y << "/" << n << "): ";
+    char c;
+    std::cin >> c;
+    if ( tolower( c ) == tolower( y ) )
+    {
+        return true;
+    }
+    else if ( tolower( c ) == tolower( n ) )
+    {
+        return false;
+    }
+    else
+    {
+        goto start_confirm;
+    }
 }
 
 #endif
