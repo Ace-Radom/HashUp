adac6a6f6359e142e85ab88219961bf712f2fe94
Ace_Radom <sichenradomlyu@gmail.com>
2023-10-09 13:58:15 +0200

    use cross-platform output streams and str

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,10 +1,10 @@
 #include"utils.h"
 
-bool rena::confirm_interrupt( const std::string& msg , char y , char n ){
+bool rena::confirm_interrupt( const CPSTR& msg , char y , char n ){
 
 start_confirm:
 
-    std::cout << msg << " (" << y << "/" << n << "): ";
+    CPOUT << msg << " (" << y << "/" << n << "): ";
     char c;
     std::cin >> c;
     if ( tolower( c ) == tolower( y ) )
