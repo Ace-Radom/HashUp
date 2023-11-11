1d7a815e8ab35f91535d531ff5056aabbdea9d14
Ace_Radom <sichenradomlyu@gmail.com>
2023-11-12 00:49:03 +0100

    finish logs in cppfignore.cpp

--- include/cppfignore.h
+++ include/cppfignore.h
@@ -4,8 +4,10 @@
 #include<iostream>
 #include<fstream>
 #include<regex>
+#include<utility>
 #include<filesystem>
 
+#include"renalog.h"
 #include"utils.h"
 
 namespace rena {
@@ -27,9 +29,10 @@ namespace rena {
             bool check( std::filesystem::path path , FILETYPE ft );
 
         private:
-            std::filesystem::path   _ifp;       // ignore file path
-            std::fstream            _rwFile;
-            std::vector<std::regex> _rlist;     // regex list
+            std::filesystem::path                          _ifp;       // ignore file path
+            std::fstream                                   _rwFile;
+            std::vector<std::pair<std::regex,std::string>> _rlist;     // regex list (pattern,original pattern str)
+            // save original str for logger (std::basic_regex does not provide .str() any more [N1499])
 
             bool _check_pp( std::filesystem::path path , FILETYPE ft );
 
